93d2d4e921ccd1ffa06c82adf452694c7503b359
Victor Manuel <victornunezpradas@gmail.com>
2023-05-09 12:49:25 +0200

    contacto finalizado

--- js/app.js
+++ js/app.js
@@ -1,16 +1,40 @@
 const modalImagen = document.querySelector("#modal-imagen");
-const contenedorImagen = modalImagen.querySelector(".modal-body");
 
-modalImagen.addEventListener("show.bs.modal", (e) => {
-  const enlace = e.relatedTarget;
-  const rutaImagen = enlace.getAttribute("data-bs-imagen");
-  const imagen = document.createElement("IMG");
-  imagen.src = `../img/${rutaImagen}.jpg`;
-  imagen.alt = `imagen galería`;
-  imagen.classList.add("img-fluid");
-  contenedorImagen.appendChild(imagen);
-});
 
-modalImagen.addEventListener("hidden.bs.modal", (e) => {
-  contenedorImagen.removeChild(contenedorImagen.firstChild);
-});
+if (modalImagen) {
+  modalImagen.addEventListener("show.bs.modal", (e) => {
+    const contenedorImagen = modalImagen.querySelector(".modal-body");
+    const enlace = e.relatedTarget;
+    const rutaImagen = enlace.getAttribute("data-bs-imagen");
+    const imagen = document.createElement("IMG");
+    imagen.src = `../img/${rutaImagen}.jpg`;
+    imagen.alt = `imagen galería`;
+    imagen.classList.add("img-fluid");
+    contenedorImagen.appendChild(imagen);
+  });
+
+  modalImagen.addEventListener("hidden.bs.modal", (e) => {
+    const contenedorImagen = modalImagen.querySelector(".modal-body");
+    contenedorImagen.removeChild(contenedorImagen.firstChild);
+  });
+}
+
+(() => {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+  console.log(forms)
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
